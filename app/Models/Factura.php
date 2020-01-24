46af1ce1371a1e7482a4977796c32754ceed022b
Gabriel Costta <gio666nb@gmail.com>
2020-01-24 16:53:36 -0500

    Agregando guardado de guia de remision: --FacturaController.php. -- FacturasAgregar.vue. Nuevo archivo: "FacturaGuiaDeRemision.php"

--- app/Models/Factura.php
+++ app/Models/Factura.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Factura extends Model
 {
-    protected $table ="factura"; 
+    protected $table = "factura";
     protected $primaryKey = 'id_factura';
-    protected $fillable =['ambiente', 'tipo_emision', 'fecha_emision', 'forma_pago', 'clave_acceso', 'observacion', 'subtotal_sin_impuesto', 'subtotal_12%', 'subtotal_0%', 'subtotal_no_obj_iva', 'descuento', 'valor_ice', 'valor_irbpnr', 'iva_12%', 'propina', 'valor_total', 'id_cliente', 'id_empresa'];
+    protected $fillable = ['ambiente', 'tipo_emision', 'fecha_emision', 'forma_pago', 'clave_acceso', 'observacion', 'subtotal_sin_impuesto', 'subtotal_12%', 'subtotal_0%', 'subtotal_no_obj_iva', 'descuento', 'valor_ice', 'valor_irbpnr', 'iva_12%', 'propina', 'valor_total', 'id_cliente', 'id_empresa'];
 }
